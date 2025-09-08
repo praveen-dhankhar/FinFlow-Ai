9211a875bbfe1aa4f67bb026e36c72ee3003b83c
Praveen Dhankhhar <praveendhankhar0@gmail.com>
2025-09-08 22:17:56 +0530

    Fix CI: add application-test/integration-test overrides; scope security headers to non-test; tune pools for CI

--- src/main/java/com/financeapp/config/SecurityHeadersConfig.java
+++ src/main/java/com/financeapp/config/SecurityHeadersConfig.java
@@ -2,6 +2,7 @@ package com.financeapp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,6 +10,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@Profile({"!test", "!integration-test"})
 public class SecurityHeadersConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
